c20a2150e2049abbeb1cd2672d79b03962dbc0b9
Nate Stedman <natesm@gmail.com>
2015-10-29 08:40:39 -0400

    Add AttributedLigature enumeration.

---
+++ Attributed/AttributedLigature.swift
@@ -0,0 +1,24 @@
+// Attributed
+// Written in 2015 by Nate Stedman <nate@natestedman.com>
+//
+// To the extent possible under law, the author(s) have dedicated all copyright and
+// related and neighboring rights to this software to the public domain worldwide.
+// This software is distributed without any warranty.
+//
+// You should have received a copy of the CC0 Public Domain Dedication along with
+// this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+/// Enumerates supported ligature settings.
+public enum AttributedLigature: Int
+{
+    /// No ligatures will be used.
+    case None = 0
+    
+    /// The default ligatures will be used
+    case Default = 1
+    
+    #if os(OSX)
+    /// All ligatures will be used.
+    case All = 2
+    #endif
+}
