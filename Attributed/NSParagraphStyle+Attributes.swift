f4875db39ea937ce61260ab5f5579ec472a5ebc3
Nate Stedman <natesm@gmail.com>
2015-11-03 21:32:48 -0400

    Add NSParagraphStyle extension.

---
+++ Attributed/NSParagraphStyle+Attributes.swift
@@ -0,0 +1,42 @@
+// Attributed
+// Written in 2015 by Nate Stedman <nate@natestedman.com>
+//
+// To the extent possible under law, the author(s) have dedicated all copyright and
+// related and neighboring rights to this software to the public domain worldwide.
+// This software is distributed without any warranty.
+//
+// You should have received a copy of the CC0 Public Domain Dedication along with
+// this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+#if os(iOS)
+    import UIKit
+#else
+    import AppKit
+#endif
+
+public extension NSParagraphStyle
+{
+    /**
+     Applies a paragraph style to the enclosed attributed strings.
+     
+     The paragraph style will not override a paragraph style set deeper n the hierarchy.
+     
+     - parameter strings: The attributed strings to apply the paragraph style to.
+     */
+    public func attribute(strings: AttributedStringConvertible...) -> AttributedStringConvertible
+    {
+        return Style(self, strings)
+    }
+    
+    /**
+     Applies a paragraph style to the enclosed attributed strings.
+     
+     The paragraph style will not override a paragraph style set deeper n the hierarchy.
+     
+     - parameter strings: The attributed strings to apply the paragraph style to.
+     */
+    public func attribute(strings: [AttributedStringConvertible]) -> AttributedStringConvertible
+    {
+        return Style(self, strings)
+    }
+}
